e867c4edce71f7a981598e81f5b07a70d399395a
moyeen_g <moyeengodyari@gmail.com>
2023-02-09 00:01:07 +0530

    secomd commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
+
 class App extends Component {
+
     render() {
+        let arr = ["abcd", "efgh", "ijkl", "mnop"];
 
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+                <ol key="relativeList">{ arr.map((value, index) => 
+                    <li key={ "relativeListItem" + (index+1) }>{ value }</li>
+                )}
+                </ol>
             </div>
         )
     }
